72d80347e3a520ff217d9aed3fc8039480cda081
syang <syang@local>
2022-08-26 16:18:10 +0800

    Initial Commit

---
+++ NewFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewFeed
+//
+//  Created by shaohua yang on 2022/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
